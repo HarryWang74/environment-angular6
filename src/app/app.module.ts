53a04fe5aa34b13a6c08fe0f127c66b7cb2e7390
harry <harry74323@gmail.com>
2018-04-15 09:03:54 +1000

    final clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AngularMaterialLibrary } from './angular-material-library/angular-material-library.module';
 
 import { AppComponent } from './app.component';
 
-import { AngularMaterialLibrary } from './angular-material-library.module';
+
 
 @NgModule({
   declarations: [
